a029f6eac274bd3188bf01637353479233869670
cgnicolas <cgnicolas@gmail.com>
2020-04-04 23:23:15 -0400

    Updated getlights

--- routes/process/get/lights/lights.js
+++ routes/process/get/lights/lights.js
@@ -2,7 +2,14 @@ const router = require('express').Router();
 const philips = require('../../../../index');
 
 router.get('/', (req, res) => {
-    res.status(200).json(philips._getLocalLights())
+    philips.getLights()
+    .then((lights) => {
+        res.status(200).json(lights);
+    })
+    .catch((err) => {
+        console.log(err);
+        res.send(err.stack);
+    })
 })
 
 module.exports = router;
